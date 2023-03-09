39e9b3fee77a8f1de0c2ad5f88d9fe42ec210928
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-03-09 00:08:53 +0000

    need to update Spotify.js end points

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,27 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+        this.search = this.search.bind(this);
+        this.handleQueryChange = this.handleQueryChange.bind(this);
+    }
+
+    search() {
+        this.props.onSearch(this.state.query);
+    }
+
+    handleQueryChange(event) {
+        this.setState({query: event.target.value});
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleQueryChange} />
                 <button className="SearchButton">SEARCH</button>
             </div>
         )
